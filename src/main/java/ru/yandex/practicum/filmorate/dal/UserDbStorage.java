2320950945bf21dd01daaed80229335f47ea639f
Irem <irem.ashfak@gmail.com>
2025-01-23 20:55:05 +0300

    fixes

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
-
 import java.util.List;
 
 @Slf4j
